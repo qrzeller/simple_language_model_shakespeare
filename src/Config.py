b40ae3298fc902229dbcc98caaed567e5a08fd5e
Quentin Zeller <q@zeller.xyz>
2025-12-12 14:10:34 +0100

    create config

---
+++ src/Config.py
@@ -0,0 +1,23 @@
+import configparser
+
+class Config:
+    N: int  # context length or max sequence length
+    @staticmethod
+    def load_from_file(filepath: str) -> "Config":
+        parser = configparser.ConfigParser()
+        parser.read(filepath)
+
+        cfg = Config()
+        # required values (will raise if missing)
+        cfg.block_size = parser.getint('DEFAULT', 'block_size')
+        cfg.N = cfg.block_size  # alias used elsewhere in the code
+
+        cfg.model_dim = parser.getint('DEFAULT', 'n_embd')
+        cfg.num_heads = parser.getint('DEFAULT', 'n_head')
+        cfg.num_layers = parser.getint('DEFAULT', 'n_layer')
+        cfg.dropout = parser.getfloat('DEFAULT', 'dropout')
+        cfg.batch_size = parser.getint('DEFAULT', 'batch_size')
+        cfg.learning_rate = parser.getfloat('DEFAULT', 'learning_rate')
+        cfg.device = parser.get('DEFAULT', 'device')
+
+        return cfg
